471636ee336c2df6de7a9da6e97906cdf6ee2d61
hyejoung <qkrgpwjd1996@gmail.com>
2025-05-26 18:11:50 +0900

    포트폴리오 작업중 1

---
+++ js_level1_portfolio/scripts/portfolio.js
@@ -0,0 +1,47 @@
+// 프로젝트소개 팝업 알고리즘
+// 1.팝업 숨기기(popup_bg)
+// 2.썸네일 이미지(a) 클릭 시 팝업 보이기(popup_bg)
+// 3.(팝업 실행 기준) 배경클릭(popup_bg)시 팝업 숨기기(popup_bg)
+// 4.프로젝트 팝업 실행 시 클릭한 대상의 이미지가 출력되는 알고리즘
+// 1.클릭한 대상의 이미지 경로 확인
+// 2.위 경로를 팝업 이미지의 경로에 대입
+//알고리즘 기준 반복해야하는 데이터를 변수로 생성하기
+const popupBg = document.querySelector('.popup_bg');//하나만 잡을때
+const thumbNailA = document.querySelectorAll('.design a');//여러개 잡을때
+console.log(popupBg,thumbNailA);//111
+//1.팝업숨기기
+popupBg.style.display = 'none';//111
+//변수로 만든 DOM요소가 여러개일 경우 이벤트를 위해 접근할때는 인덱스를 사용해서 하나씩 개별 접근해야한다.
+// 2.썸네일 이미지(a) 클릭 시 팝업 보이기(popup_bg)
+thumbNailA[0].addEventListener('click',function(){
+    console.log(0);
+    console.log(this);
+    console.log(this.children[0].src);
+    console.log(popupBg.children[0].children[0].src); //팝업bg의 자식의 자식의 src 확인
+    popupShow(); //팝업 출력 함수 호출
+    popupBg.children[0].children[0].src = this.children[0].src
+})
+thumbNailA[1].addEventListener('click',function(){
+    console.log(0);
+    console.log(this);
+    console.log(this.children[0].src);
+    console.log(popupBg.children[0].children[0].src); //팝업bg의 자식의 자식의 src 확인
+    popupShow(); //팝업 출력 함수 호출
+    popupBg.children[0].children[0].src = this.children[0].src
+})
+thumbNailA[2].addEventListener('click',function(){
+    console.log(0);
+    console.log(this);
+    console.log(this.children[0].src);
+    console.log(popupBg.children[0].children[0].src); //팝업bg의 자식의 자식의 src 확인
+    popupShow(); //팝업 출력 함수 호출
+    popupBg.children[0].children[0].src = this.children[0].src
+})
+function popupShow(){
+    return popupBg.style.display = 'flex';
+}
+popupBg.addEventListener('click',popupHide)
+function popupHide(){
+    return popupBg.style.display = 'none';
+}
+// 3.(팝업 실행 기준) 배경클릭(popup_bg)시 팝업 숨기기(popup_bg)
